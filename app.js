1dc9b7e1e289a60be6b2c79436e3ee42b4d008a9
Sagar <04017711721_iot@vips.edu>
2023-08-20 19:20:21 +0530

    Backend of User is Done

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { UserRoutes } from './routes/userRoutes.js';
 import {IdeRoutes} from './routes/IdeRoutes.js';
+import cors from 'cors';
 
 // to up the server 
 const app = express();
+
+
+// Handling with the CORS ----> cross origin resource sharing {MIDDLE WARE }
+app.use(cors());
 
 // Fetching all the data  (and next to the middle ware )
 app.use(express.json())
